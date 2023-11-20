3df2ca7b865aa51ec51b6b11b6bcc2138980f09c
Pierre-Antoine <patremblay22@gmail.com>
2023-11-20 10:01:45 +0100

    End of Section 19.2

---
+++ client/src/app/shared/models/order.ts
@@ -0,0 +1,29 @@
+import { Address } from "./user"
+
+export interface OrderToCreate {
+    basketId: string;
+    deliveryMethodId: number;
+    shipToAddress: Address;
+}
+
+export interface OrderItem {
+  productId: number;
+  productTitle: string;
+  pictureUrl1: string;
+  price: number;
+  quantity: number;
+}
+export interface Order {
+    id: number;
+    buyerEmail: string;
+    orderDate: Date;
+    shipToAddress: Address;
+    deliveryMethod: string;
+    shippingPrice: number;
+    orderItems: OrderItem[];
+    subtotal: number;
+    total: number;
+    status: string;
+  }
+  
+  
